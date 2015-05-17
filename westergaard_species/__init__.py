0228200a15a1c43155323270f428b4cc4547d59c
Your Name <you@example.com>
2015-05-17 13:24:42 +0100

    added convenience functions; final bar line is now customizable

--- westergaard_species/__init__.py
+++ westergaard_species/__init__.py
@@ -1,8 +1,11 @@
 __all__ = ['lilypondfile_from_yaml',
-           'score_from_steps', 'score_from_yaml',
-           'steps_from_yaml']
+           'score_from_steps', 'score_from_yaml', 'steps_from_yaml',
+           'export_from_yaml', 'export_preview_from_yaml', 'export_from_yaml_',
+           'ishow_from_yaml', 'ishow_preview_from_yaml', 'ishow_from_yaml_']
 
 from westergaard_species import lilypondfile_from_yaml, \
-        score_from_steps, score_from_yaml, steps_from_yaml
+        score_from_steps, score_from_yaml, steps_from_yaml, \
+        export_from_yaml, export_preview_from_yaml, export_from_yaml_, \
+        ishow_from_yaml, ishow_preview_from_yaml, ishow_from_yaml_
 
 
